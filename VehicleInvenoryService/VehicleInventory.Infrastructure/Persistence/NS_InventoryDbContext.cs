629990d426888e6a3a1ae2ecb47b9eb50fee6971
Nirav04-hub <nsaxena9968@conestogac.on.ca>
2026-02-16 19:06:46 -0500

    Added DbContext for vehicles

--- VehicleInvenoryService/VehicleInventory.Infrastructure/Persistence/NS_InventoryDbContext.cs
+++ VehicleInvenoryService/VehicleInventory.Infrastructure/Persistence/NS_InventoryDbContext.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleInventory.Domain.Entities;
 
 namespace VehicleInventory.InfrastructureNS.Persistence
 {
-    public class NS_InventoryDbContext
+    public class NS_InventoryDbContext : DbContext
     {
+        public NS_InventoryDbContext   (DbContextOptions<NS_InventoryDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Vehicle>(entity =>
+            {
+                entity.HasKey(v => v.Id);
+                entity.Property(v => v.VehicleCode).IsRequired().HasMaxLength(100);
+                entity.Property(v => v.VehicleType).IsRequired().HasMaxLength(100);
+                entity.Property(v => v.LocationId).IsRequired();
+                entity.Property(v => v.Status).IsRequired().HasColumnType("decimal(18,2)");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
